b7670af3d1d9eee288c01d4460d51b8cbe0833f3
vskumov <v.kumov@gmail.com>
2024-02-21 14:46:20 +0100

    task 8 done

--- 08.js
+++ 08.js
@@ -1,10 +1,22 @@
 function myEach(array, action) {
     for (let i = 0; i < array.length; i++) {
-        action(array[i]);
+       action(array[i]);
     }
-}
+ }
+ 
+ function myFilter(array, predicate) {
+    let result = [];
+    myEach(array, function(element) {
+       if (predicate(element)) {
+          result.push(element);
+       }
+    });
+    return result;
+ }
+ 
+ function notTwo(element) {
+    return element !== 2;
+ }
 
-myEach([1, 2, 3], console.log);
-
-
-
+ let filtered = myFilter([1,2,3,4,2,4,6,22,464,3,2,21], notTwo);
+ console.log(filtered)
