5d538cb432eb85ade64955d8dcd42a0994acd725
Your Name <you@example.com>
2024-02-21 15:04:06 +0100

    task 9 done

--- 09.js
+++ 09.js
@@ -5,3 +5,4 @@ function merge(obj1, obj2) {
 
 console.log(merge({ a: 3, b: 2 }, { a: 2, c: 4 })) 
 console.log(merge({ a: 5, b: 2 }, { a: 6, c: 4 }))
+console.log(merge({ a: 5, b: 2, c: 9}, { a: 6, b: 5,  c: 4 }))
